c58fb26df894873323f7c7180ba3017ac1a4ace4
Merrone <200412392+strxwberrylxvver@users.noreply.github.com>
2025-11-23 23:25:35 +0000

    Adding styles to buttons

--- src/components/entities/transactions/TransactionsTable.js
+++ src/components/entities/transactions/TransactionsTable.js
@@ -1,9 +1,18 @@
 import "./TransactionsTable.css";
 
 function TransactionsTable({ transactions }) {
+
+  // const handleAdd = () => setShowNewModuleForm
+
   return (
     <div className="transactionBox">
+      <div className="tableHeader">
       <h2>Spending History</h2>
+      {/* <Action.Tray>
+        <Action.Add showText onClick = {handleAdd} />
+      </Action.Tray> */}
+      <button> + </button>
+      </div>
       <table className="transactionTable">
         <thead>
           <tr>
