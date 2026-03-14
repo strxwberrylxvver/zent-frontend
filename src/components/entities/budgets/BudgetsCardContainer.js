eba75af3a6ae9bbc0b90ac52a14db5e56ad9ea44
Merrone <200412392+strxwberrylxvver@users.noreply.github.com>
2026-03-14 23:59:46 +0000

    Refactor: removed redudant actions prop and replaced with working Action in BudgetsCardContainer

--- src/components/entities/budgets/BudgetsCardContainer.js
+++ src/components/entities/budgets/BudgetsCardContainer.js
@@ -1,10 +1,44 @@
 import BudgetsCard from "./BudgetsCard";
+import CalendarIcon from "../../assets/icons/calendar.png";
+import SortIcon from "../../assets/icons/sorting.png";
+import FilterIcon from "../../assets/icons/filter.png";
+import Action from "../../UI/Actions";
 import "./BudgetsCardContainer.css";
 
-function BudgetsCardContainer({ budgets, onSelect, actions }) {
+function BudgetsCardContainer({ budgets, onSelect, onClick }) {
   return (
     <>
-      <div className="actionTray">{actions}</div>
+      <div className="topBar">
+        <div className="sorters">
+          <div className="iconOutline">
+            <img src={CalendarIcon} alt="Calendar" className="icon" />
+          </div>
+          <div className="iconOutline">
+            <p>This month</p>
+          </div>
+          <div className="iconOutline">
+            <img src={SortIcon} alt="Sort" className="icon" />
+          </div>
+          <div className="iconOutline">
+            <p>Sort by: Default </p>
+          </div>
+          <div className="iconOutline">
+            <img src={FilterIcon} alt="Filter" className="icon" />
+          </div>
+          <div className="reset">
+            <p>Reset all </p>
+          </div>
+        </div>
+        <div className="addButton">
+          <Action.Tray>
+            <Action.Add
+              showText
+              buttonText="+ Add new budget "
+              onClick={onClick}
+            />
+          </Action.Tray>
+        </div>
+      </div>
       <div className="budgetsCardContainer">
         {budgets.map((budget) => (
           <BudgetsCard
--- src/components/entities/budgets/BudgetsCrudler.js
+++ src/components/entities/budgets/BudgetsCrudler.js
@@ -6,9 +6,9 @@ import BudgetView from "./BudgetView.js";
 import { useModal, Modal } from "../../UI/Modal.js";
 import BudgetsCardContainer from "./BudgetsCardContainer.js";
 import Action from "../../UI/Actions.js";
-import CalendarIcon from "../../assets/icons/calendar.png";
-import SortIcon from "../../assets/icons/sorting.png";
-import FilterIcon from "../../assets/icons/filter.png";
+// import CalendarIcon from "../../assets/icons/calendar.png";
+// import SortIcon from "../../assets/icons/sorting.png";
+// import FilterIcon from "../../assets/icons/filter.png";
 import BudgetGauge from "./BudgetGauge";
 import "./BudgetsCrudler.css";
 
@@ -45,7 +45,7 @@ function BudgetsCrudler({ endpoint, showTitle = true }) {
         <div className="contentArea">
           <div className="mainSection">
             <div className="topBar">
-              <div className="sorters">
+              {/* <div className="sorters">
                 <div className="iconOutline">
                   <img src={CalendarIcon} alt="Calendar" className="icon" />
                 </div>
@@ -64,8 +64,8 @@ function BudgetsCrudler({ endpoint, showTitle = true }) {
                 <div className="reset">
                   <p>Reset all </p>
                 </div>
-              </div>
-              <div className="addButton">
+              </div> */}
+              {/* <div className="addButton">
                 <Action.Tray>
                   <Action.Add
                     showText
@@ -73,14 +73,18 @@ function BudgetsCrudler({ endpoint, showTitle = true }) {
                     onClick={handleAdd}
                   />
                 </Action.Tray>
-              </div>
+              </div> */}
             </div>
             {!budgets ? (
               <p>{loadingMessage}</p>
             ) : budgets.length === 0 ? (
               <p>No goals.</p>
             ) : (
-              <BudgetsCardContainer budgets={budgets} onSelect={handleSelect} />
+              <BudgetsCardContainer
+                budgets={budgets}
+                onSelect={handleSelect}
+                onClick={handleAdd}
+              />
             )}
             {showDetails && (
               <Modal
