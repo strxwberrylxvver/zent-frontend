e54ade119cf6b39168a759658f645828d308de36
Merrone <200412392+strxwberrylxvver@users.noreply.github.com>
2026-03-16 12:43:26 +0000

    Updated NavBar and Layout styling

--- src/components/layout/NavBar.js
+++ src/components/layout/NavBar.js
@@ -7,39 +7,56 @@ import AnalyticsIcon from "../assets/icons/AnalyticsIcon.png";
 import HouseholdIcon from "../assets/icons/HouseholdIcon.png";
 
 function NavBar() {
-  const getLinkStyle = ({ isActive }) => (isActive ? "navSelected" : null);
+  const getLinkStyle = ({ isActive }) => (isActive ? "navLink navSelected" : "navLink");
 
   return (
     <nav>
-      <div className="navItem">
-        <NavLink to="/dashboard" className={getLinkStyle}>
-          <img src={DashboardIcon} alt="dashboard" className="nav-icon" />
-          <h2 className="nav-text">DashBoard</h2>
-        </NavLink>
+      <div className="navLinks">
+        <div className="navItem">
+          <NavLink to="/dashboard" className={getLinkStyle}>
+            <img src={DashboardIcon} alt="dashboard" className="nav-icon" />
+            <h2 className="nav-text">Dashboard</h2>
+          </NavLink>
+        </div>
+        <div className="navItem">
+          <NavLink to="/budgets" className={getLinkStyle}>
+            <img src={BudgetsIcon} alt="budgets" className="nav-icon" />
+            <h2 className="nav-text">Budgets</h2>
+          </NavLink>
+        </div>
+        <div className="navItem">
+          <NavLink to="/goals" className={getLinkStyle}>
+            <img src={GoalsIcon} alt="goals" className="nav-icon" />
+            <h2 className="nav-text">Goals</h2>
+          </NavLink>
+        </div>
+        <div className="navItem">
+          <NavLink to="/analytics" className={getLinkStyle}>
+            <img src={AnalyticsIcon} alt="analytics" className="nav-icon" />
+            <h2 className="nav-text">Analytics</h2>
+          </NavLink>
+        </div>
+        <div className="navItem">
+          <NavLink to="/sharedbills" className={getLinkStyle}>
+            <img src={HouseholdIcon} alt="shared bills" className="nav-icon" />
+            <h2 className="nav-text">Shared Bills</h2>
+          </NavLink>
+        </div>
       </div>
-      <div className="navItem">
-        <NavLink to="/budgets" className={getLinkStyle}>
-          <img src={BudgetsIcon} alt="budgets" className="nav-icon" />
-          <h2 className="nav-text">Budgets</h2>
-        </NavLink>
-      </div>
-      <div className="navItem">
-        <NavLink to="/goals" className={getLinkStyle}>
-          <img src={GoalsIcon} alt="goals" className="nav-icon" />
-          <h2 className="nav-text">Goals</h2>
-        </NavLink>
-      </div>
-      <div className="navItem">
-        <NavLink to="/analytics" className={getLinkStyle}>
-          <img src={AnalyticsIcon} alt="analytics" className="nav-icon" />
-          <h2 className="nav-text">Analytics</h2>
-        </NavLink>
-      </div>
-      <div className="navItem">
-        <NavLink to="/sharedbills" className={getLinkStyle}>
-          <img src={HouseholdIcon} alt="shared bills" className="nav-icon" />
-          <h2 className="nav-text">Shared Bills</h2>
-        </NavLink>
+
+      <div className="navBottom">
+        <div className="navItem">
+          <NavLink to="/contact" className={getLinkStyle}>
+            <span className="nav-icon navBottomIcon">?</span>
+            <h2 className="nav-text">Help</h2>
+          </NavLink>
+        </div>
+        <div className="navItem">
+          <NavLink to="/signin" className={getLinkStyle}>
+            <span className="nav-icon navBottomIcon">↪</span>
+            <h2 className="nav-text">Log out</h2>
+          </NavLink>
+        </div>
       </div>
     </nav>
   );
