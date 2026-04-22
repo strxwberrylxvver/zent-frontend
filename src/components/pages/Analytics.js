b755d9ab9d1e2508f099a7307050d5b2f26e4b12
Merrone <200412392+strxwberrylxvver@users.noreply.github.com>
2026-04-22 02:54:22 +0100

    feat: updated styles

--- src/components/pages/Analytics.js
+++ src/components/pages/Analytics.js
@@ -1,15 +1,14 @@
 import AnalyticsCrudler from "../entities/analytics/AnalyticsCrudler.js";
 import "../../global.css";
+
 function Analytics() {
   return (
     <section>
-      <section className="budgetsPageView">
-        <h1>Analytics</h1>
-        <div className="caption">
-          <p>A breakdown of your financial activity</p>
-        </div>
-        <AnalyticsCrudler />
-      </section>
+      <h1>Analytics</h1>
+      <div className="caption">
+        <p>A breakdown of your financial activity</p>
+      </div>
+      <AnalyticsCrudler />
     </section>
   );
 }
